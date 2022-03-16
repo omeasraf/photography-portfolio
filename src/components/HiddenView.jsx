adb62b666410460c4dd75c8617e38f3acec91674
Ome Asraf <omeasraf0@gmail.com>
2022-03-16 17:26:20 -0400

    Added temp file info

--- src/components/HiddenView.jsx
+++ src/components/HiddenView.jsx
@@ -1,23 +1,46 @@
 const HiddenView = () => {
   return (
-    <div className="hidden-view">
-      <div className="items">
-        <div className="image">
-          <img
-            src={require("../assets/proposal_1.JPG")}
-            alt="Proposal 1"
-            width="100%"
-          />
+    <div className="itemsView">
+      <div className="hidden-view">
+        <div className="items">
+          <div className="image">
+            <img
+              src={require("../assets/proposal_1.JPG")}
+              alt="Proposal 1"
+              width="100%"
+            />
+          </div>
+          <div className="info">
+            <h3>March 13, 2022</h3>
+            <a
+              className="download"
+              href={require("../assets/proposal_1.JPG")}
+              download
+            >
+              Download
+            </a>
+          </div>
         </div>
-        <div className="info">
-          <h3>March 13, 2022</h3>
-          <a
-            className="download"
-            href={require("../assets/proposal_1.JPG")}
-            download
-          >
-            Download
-          </a>
+      </div>
+      <div className="mt-[5vh]">
+        <div className="flex flex-col justify-center items-center">
+          <h3>Download Zip Files</h3>
+          <div className="flex flex-row gap-5">
+            <a
+              className="download"
+              href={require("../assets/zips/Proposal.zip")}
+              download
+            >
+              Download Edited Files
+            </a>
+            <a
+              className="download"
+              href={require("../assets/zips/Unedited.zip")}
+              download
+            >
+              Download Unedited Files
+            </a>
+          </div>
         </div>
       </div>
     </div>
