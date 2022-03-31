1d355b151ec5604a9c35cade9f06a8d949f72bbb
Ome Asraf <omeasraf0@gmail.com>
2022-03-31 17:38:10 -0400

    Added bg to contact view

--- src/containers/ContactView.jsx
+++ src/containers/ContactView.jsx
@@ -2,19 +2,26 @@ import { BsInstagram, BsEnvelope } from "react-icons/bs";
 
 export const ContactView = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-[100vh] w-full">
+    <div className="contactView flex flex-col justify-center items-center h-[100vh] w-full">
       <div className="flex flex-col gap-6">
         <a
           href="https://instagram.com/photobyabhi"
           target="_blank"
           rel="noreferrer"
         >
-          <h2 className="flex flex-row gap-5 text-[35px] hover:text-[#ff3549]">
+          <h2
+            className="flex flex-row gap-5 text-[35px] hover:text-[#ff3549]"
+            data-aos="fade-up"
+          >
             <BsInstagram /> Instagram
           </h2>
         </a>
         <a href="mailto:abhiasraf01@gmail.com" target="_blank" rel="noreferrer">
-          <h2 className="flex flex-row gap-5 text-[35px] hover:text-[#35c6ff]">
+          <h2
+            className="flex flex-row gap-5 text-[35px] hover:text-[#35c6ff]"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
             <BsEnvelope /> Email
           </h2>
         </a>
